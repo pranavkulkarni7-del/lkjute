605dfe5ad19f919c05781b31c054a8b4b11890fa
pranavkulkarni7-del <pranavkulkarni834@gmail.com>
2026-01-18 19:52:54 +0530

    Update bsdk.py

---
+++ bsdk.py
@@ -0,0 +1,9 @@
+emp_name = "Pranav Kulkarni"
+emp_id = "01FE24BCA173"
+emp_age = 26
+emp_salary = 55000
+
+print("Employee Name:", emp_name)
+print("Employee ID:", emp_id)
+print("Employee Age:", emp_age)
+print("Employee Salary:", emp_salary)
